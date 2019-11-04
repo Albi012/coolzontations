fbd6f6ab92792a5b08fb2230510db91915e6d9f4
Albi012 <mayeralbert12@gmail.com>
2019-11-04 13:38:36 +0100

    Initial commit

---
+++ src/test/java/com/codecool/coolzontations/CoolzontationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.coolzontations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolzontationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
