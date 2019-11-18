82156300a6766913b0123c8ffbe844ea0974e7b4
Albi012 <mayeralbert12@gmail.com>
2019-11-18 13:10:11 +0100

    Refactor DataFromRequest with Lombok.

--- src/main/java/com/codecool/coolzontations/model/DataFromRequest.java
+++ src/main/java/com/codecool/coolzontations/model/DataFromRequest.java
@@ -1,34 +1,17 @@
 package com.codecool.coolzontations.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Component
 public class DataFromRequest {
 
     private Integer userID;
     private Integer consultationID;
-
-    public DataFromRequest() {
-    }
-
-    public DataFromRequest(Integer userID, Integer consultationID) {
-        this.userID = userID;
-        this.consultationID = consultationID;
-    }
-
-    public Integer getUserID() {
-        return userID;
-    }
-
-    public void setUserID(Integer userID) {
-        this.userID = userID;
-    }
-
-    public Integer getConsultationID() {
-        return consultationID;
-    }
-
-    public void setConsultationID(Integer consultationID) {
-        this.consultationID = consultationID;
-    }
 }
