585565da83856a58c08be46b4fc415277d6623dd
Sleepy026 <mrbrowngarden@gmail.com>
2019-12-04 10:25:00 +0100

    login / registration hotfix

--- src/main/java/com/codecool/coolzontations/controller/AuthController.java
+++ src/main/java/com/codecool/coolzontations/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.codecool.coolzontations.controller;
 
 import com.codecool.coolzontations.model.UserCredentials;
+import com.codecool.coolzontations.model.UserModel;
 import com.codecool.coolzontations.repository.UserModelRepository;
 import com.codecool.coolzontations.security.JwtTokenService;
+import com.codecool.coolzontations.service.DataManger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -10,16 +13,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -27,6 +29,9 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
 
     private final JwtTokenService jwtTokenServices;
+
+    @Autowired
+    private DataManger dataManger;
 
     public AuthController(AuthenticationManager authenticationManager, JwtTokenService jwtTokenServices, UserModelRepository users) {
         this.authenticationManager = authenticationManager;
@@ -46,11 +51,16 @@ public class AuthController {
 
             String token = jwtTokenServices.createToken(username, roles);
 
-            Map<Object, Object> model = new HashMap<>();
-            model.put("username", username);
-            model.put("roles", roles);
-            model.put("token", token);
-            return ResponseEntity.ok(model);
+            Optional<UserModel> user = dataManger.findUserByUsername(username);
+
+
+                Map<Object, Object> model = new HashMap<>();
+                model.put("username", user.get().getUsername());
+                model.put("level", user.get().getLevel());
+                model.put("userID", user.get().getId());
+                model.put("token", token);
+                return ResponseEntity.ok(model);
+
         } catch (AuthenticationException e) {
             throw new BadCredentialsException("Invalid username/password supplied");
         }
